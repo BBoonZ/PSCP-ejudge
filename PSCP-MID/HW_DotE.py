1b85b517fba313e8bd38609b2f217dfee5820cc0
BBoonZ <bosszaza0380@gmail.com>
2023-11-07 22:33:30 +0700

    Update HW_DotE.py

--- PSCP-MID/HW_DotE.py
+++ PSCP-MID/HW_DotE.py
@@ -4,5 +4,5 @@ def main(player):
     """i dont know to do"""
     if player == 1:
         return print(2)
-    print((player+player-1)*2)
+    print(math.factorial(player)//math.factorial(player//2)*2)
 main(int(input()))
